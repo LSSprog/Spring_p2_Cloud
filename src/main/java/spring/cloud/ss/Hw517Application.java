58f4f8f5d4caa315b1d1a048a2fa2bf321a8055a
LSSprog <fedexss@bk.ru>
2021-03-12 17:52:57 +0300

    first commit

---
+++ src/main/java/spring/cloud/ss/Hw517Application.java
@@ -0,0 +1,13 @@
+package spring.cloud.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw517Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/cloud/ss/Hw517ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw517ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
